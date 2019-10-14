6d5b8c709eb883e6785a9e21d93b6a9b4f88669b
michal-dom <michal-domagala@hotmail.com>
2019-10-14 13:54:31 +0200

    python changes

--- MainLab01.py
+++ MainLab01.py
@@ -1,31 +1,37 @@
+# author Wojciech Jakub Jargielo & Michal Domagała
+
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
-
 
 def readcsv(filename):
     data = pd.read_csv(filename)
     return np.array(data)
 
-
-def standaryzacja(csv_data):
+def standarization(data):
     scaler = StandardScaler()
-    print(scaler.fit(csv_data))
+    scaler.fit(data)
     print(scaler.mean_)
-    print(scaler.transform(csv_data))
-
+    return scaler.transform(data)
 
 def minMax(data):
     scaler = MinMaxScaler()
-    print(scaler.fit(csv_data))
+    scaler.fit(data)
     print(scaler.data_max_)
-    print(scaler.transform(data))
+    return scaler.transform(data)
 
+def KNN(X, y):
+    neigh = KNeighborsClassifier(n_neighbors=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+    neigh.fit(X_train, y_train)
+    score = neigh.score(X_test, y_test)
+    print(score)
 
 if __name__ == "__main__":
-    csv_data = readcsv('mnist_train.csv');
-    print("-------standaryzacja--------------")
-    standaryzacja(csv_data)
-    print("-------minMax--------------")
-    minMax(csv_data)
+    data = readcsv("mnist_train.csv")
+    y = 'classes'  # to do - get classes from csv
+    KNN(standarization(data), y)
+    KNN(minMax(data), y)
