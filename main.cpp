e7896aeeaaa4673fcfe8b7e898e82603126cf09a
michal-dom <michal-domagala@hotmail.com>
2019-10-28 17:49:44 +0100

    mpi - normalization test v3

--- main.cpp
+++ main.cpp
@@ -84,95 +84,95 @@ void normalize(const int * array, double * normalizedArray){
 
 }
 
-void standarize(const int * array, double * normalizedArray){
-
-    for(int i = 0; i < COLS; i++){
-        standardization(i, array, normalizedArray);
-    }
-}
-
-int manhattan(double * train, double * test, int testRow){
-
-    auto minVal = DBL_MAX;
-    auto * voting = new double[10];
-
-    int classIndex = 0;
-    for(int i = 0; i < ROWS; i++){
-        double sum = 0.0;
-        for(int j = 0; j < COLS; j++){
-            int indexTrain = 784*i + j;
-            int indexTest = 784*testRow + j;
-            sum += fabs(train[indexTrain] - test[indexTest]);
-        }
-        if(minVal > sum){
-            minVal = sum;
-            classIndex = i;
-        }
-    }
-    return classIndex;
-}
-
-int chebyshev(double * train, double * test, int testRow){
-
-    auto minVal = DBL_MAX;
-    int classIndex = 0;
-    for(int i = 0; i < ROWS; i++){
-        double maxVal = 0.0;
-
-        for(int j = 0; j < COLS; j++){
-            int indexTrain = 784*i + j;
-            int indexTest = 784*testRow + j;
-
-            double tmp = fabs(train[indexTrain] - test[indexTest]);
-            if(maxVal < tmp) maxVal = tmp;
-        }
-        if(minVal > maxVal){
-            minVal = maxVal;
-            classIndex = i;
-        }
-    }
-    return classIndex;
-}
-
-
-
-int euclidian(double * train, double * test, int testRow){
-
-    auto minVal = DBL_MAX;
-    int classIndex = 0;
-    for(int i = 0; i < ROWS; i++){
-        double sum = 0.0;
-        for(int j = 0; j < COLS; j++){
-            int indexTrain = 784*i + j;
-            int indexTest = 784*testRow + j;
-            sum += (train[indexTrain] - test[indexTest]) * (train[indexTrain] - test[indexTest]);
-        }
-        sum = sqrt(sum);
-        if(minVal > sum){
-            minVal = sum;
-            classIndex = i;
-        }
-    }
-    return classIndex;
-}
-
-void knn(double * train, double * test, const int * classesTrain, const int * classesTest){
-
-
-    auto hits = 0;
-    for(int i = 0; i < 2000; i++){
-        int predClass = classesTrain[chebyshev(train, test, i)];
-        if(predClass == classesTest[i]){
-            hits++;
-        }
-    }
-
-    auto accuracy = (double) hits;
-    accuracy = accuracy/20;
-
-    cout << "Classifier accuracy is " << accuracy << "%." << endl;
-
-}
+//void standarize(const int * array, double * normalizedArray){
+//
+//    for(int i = 0; i < COLS; i++){
+//        standardization(i, array, normalizedArray);
+//    }
+//}
+//
+//int manhattan(double * train, double * test, int testRow){
+//
+//    auto minVal = DBL_MAX;
+//    auto * voting = new double[10];
+//
+//    int classIndex = 0;
+//    for(int i = 0; i < ROWS; i++){
+//        double sum = 0.0;
+//        for(int j = 0; j < COLS; j++){
+//            int indexTrain = 784*i + j;
+//            int indexTest = 784*testRow + j;
+//            sum += fabs(train[indexTrain] - test[indexTest]);
+//        }
+//        if(minVal > sum){
+//            minVal = sum;
+//            classIndex = i;
+//        }
+//    }
+//    return classIndex;
+//}
+//
+//int chebyshev(double * train, double * test, int testRow){
+//
+//    auto minVal = DBL_MAX;
+//    int classIndex = 0;
+//    for(int i = 0; i < ROWS; i++){
+//        double maxVal = 0.0;
+//
+//        for(int j = 0; j < COLS; j++){
+//            int indexTrain = 784*i + j;
+//            int indexTest = 784*testRow + j;
+//
+//            double tmp = fabs(train[indexTrain] - test[indexTest]);
+//            if(maxVal < tmp) maxVal = tmp;
+//        }
+//        if(minVal > maxVal){
+//            minVal = maxVal;
+//            classIndex = i;
+//        }
+//    }
+//    return classIndex;
+//}
+//
+//
+//
+//int euclidian(double * train, double * test, int testRow){
+//
+//    auto minVal = DBL_MAX;
+//    int classIndex = 0;
+//    for(int i = 0; i < ROWS; i++){
+//        double sum = 0.0;
+//        for(int j = 0; j < COLS; j++){
+//            int indexTrain = 784*i + j;
+//            int indexTest = 784*testRow + j;
+//            sum += (train[indexTrain] - test[indexTest]) * (train[indexTrain] - test[indexTest]);
+//        }
+//        sum = sqrt(sum);
+//        if(minVal > sum){
+//            minVal = sum;
+//            classIndex = i;
+//        }
+//    }
+//    return classIndex;
+//}
+//
+//void knn(double * train, double * test, const int * classesTrain, const int * classesTest){
+//
+//
+//    auto hits = 0;
+//    for(int i = 0; i < 2000; i++){
+//        int predClass = classesTrain[chebyshev(train, test, i)];
+//        if(predClass == classesTest[i]){
+//            hits++;
+//        }
+//    }
+//
+//    auto accuracy = (double) hits;
+//    accuracy = accuracy/20;
+//
+//    cout << "Classifier accuracy is " << accuracy << "%." << endl;
+//
+//}
 
 void readUnprocessed(int * array, int * classes){
     ifstream input("mnist_train.csv");
